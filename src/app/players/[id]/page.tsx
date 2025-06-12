a7caa3a2878b4369327110717590e39530015b55
ahicks199023 <antoinehickssales@gmail.com>
2025-06-12 17:36:31 -0500

    Create page.tsx

---
+++ src/app/players/[id]/page.tsx
@@ -0,0 +1,14 @@
+// src/app/leagues/[leagueId]/players/[playerId]/page.tsx
+
+import { useParams } from 'next/navigation'
+
+export default function PlayerPage() {
+  const { leagueId, playerId } = useParams()
+
+  return (
+    <div>
+      <h1>Player #{playerId} from League #{leagueId}</h1>
+      {/* Later: Show stats, charts, game logs, etc. */}
+    </div>
+  )
+}
