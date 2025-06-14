4c1c3eb581cb579cebe07c1f40d76de049ab5b4c
ahicks199023 <antoinehickssales@gmail.com>
2025-06-14 13:47:36 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx}', // ✅ Picks up anything in /app
     './src/pages/**/*.{js,ts,jsx,tsx}', // If you're also using /pages
     './src/components/**/*.{js,ts,jsx,tsx}', // ✅ Picks up components
+    './src/Hooks/**/*.{js,ts,jsx,tsx}',
+    './src/layouts/**/*.{js,ts,jsx,tsx}',
+    './src/lib/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
