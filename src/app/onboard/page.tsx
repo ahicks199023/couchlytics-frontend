0b781982616c6a262e911b0a111be6019195fd3d
ahicks199023 <antoinehickssales@gmail.com>
2025-06-27 00:10:53 -0500

    Update page.tsx

--- src/app/onboard/page.tsx
+++ src/app/onboard/page.tsx
@@ -1,14 +1,12 @@
 'use client'
 
-import LeagueOnboardingForm from '@/components/LeagueOnboardingForm'
-import ProtectedRoute from '@/components/ProtectedRoute'
+import UserOnboarding from '../../components/UserOnboarding'
+import ProtectedRoute from '../../components/ProtectedRoute'
 
 export default function OnboardPage() {
   return (
     <ProtectedRoute>
-      <div className="min-h-screen p-6">
-        <LeagueOnboardingForm />
-      </div>
+      <UserOnboarding />
     </ProtectedRoute>
   )
 }
