b83906aabcd9b88698b498d3e126aeb458bf1a66
ahicks199023 <antoinehickssales@gmail.com>
2025-06-28 20:53:17 -0500

    Update config.ts

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,2 +1,2 @@
 // lib/config.ts
-export const API_BASE = process.env.NEXT_PUBLIC_API_BASE || 'http://localhost:5000';
+export const API_BASE = process.env.NEXT_PUBLIC_API_BASE || 'https://api.couchlytics.com';
